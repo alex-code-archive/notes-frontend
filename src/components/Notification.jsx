cbdcd3106642625850e03771b6c4ec8f5a283d5d
Alex <alexhewitt.j@gmail.com>
2024-04-02 07:15:27 -0500

    Initial commit

---
+++ src/components/Notification.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/prop-types */
+function Notification({ message }) {
+	if (message === null) return null
+	return <div className='error'>{message}</div>
+}
+
+export default Notification
